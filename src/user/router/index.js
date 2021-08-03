bb6fd02c6bd1c4e3dc3d85a21c405e796c9e4e55
Mysh3ll <peacefulloijkn@gmail.com>
2021-08-03 22:24:49 +0200

    :white_check_mark: test: express validator

--- src/user/router/index.js
+++ src/user/router/index.js
@@ -1,39 +1,23 @@
 import express from 'express';
 import UserService from '../service';
+import { check, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-const validateUsername = (req, res, next) => {
-  const user = req.body;
-  if (user.username === null) {
-    req.validationErrors = {
-      username: 'Username cannot be null',
-    };
+router.post(
+  '/api/1.0/users',
+  check('username').notEmpty().withMessage('Username cannot be null'),
+  check('email').notEmpty().withMessage('Email cannot be null'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
+      return res.status(400).send({ validationErrors });
+    }
+    await UserService.save(req.body);
+    return res.send({ message: 'User created' });
   }
-  next();
-};
-
-const validateEmail = (req, res, next) => {
-  const user = req.body;
-  if (user.email === null) {
-    req.validationErrors = {
-      ...req.validationErrors,
-      email: 'Email cannot be null',
-    };
-  }
-  next();
-};
-
-router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
-  if (req.validationErrors) {
-    return res.status(400).send({
-      validationErrors: {
-        ...req.validationErrors,
-      },
-    });
-  }
-  await UserService.save(req.body);
-  return res.send({ message: 'User created' });
-});
+);
 
 export default router;
